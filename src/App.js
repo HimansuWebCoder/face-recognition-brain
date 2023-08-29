348fc461a2f960c1daa880c28cdbf000f6accf49
Hima-123797 <113868318+Hima-123797@users.noreply.github.com>
2023-08-29 20:16:54 +0500

    facerecognitionbrain

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+import Navigation from "./components/Navigation/Navigation.js";
+import Logo from "./components/Logo/Logo.js";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
+import Rank from "./components/Rank/Rank.js";
+import "./App.css";
+
+class App extends Component {
+  particlesInit = async (engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  };
+
+  particlesLoaded = async (container) => {
+    console.log(container);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className="particles"
+          id="tsparticles"
+          init={this.particlesInit}
+          loaded={this.particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#0d47a1",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 3,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 100,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
