730c9a44c65dfd003f329b8c5f3edf0b76f1d962
Hima-123797 <113868318+Hima-123797@users.noreply.github.com>
2023-09-23 13:34:26 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,112 @@
 import React, { Component } from "react";
 import Particles from "react-particles";
+// import Clarifai from "clarifai";
 import { loadSlim } from "tsparticles-slim";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition.js";
 import Navigation from "./components/Navigation/Navigation.js";
 import Logo from "./components/Logo/Logo.js";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./components/Rank/Rank.js";
 import "./App.css";
 
+// const app = new Clarifai.App({
+//   apiKey: "ce9ef6c6659f4308b2ee4715996acbaf",
+// });
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = "74b26640306549aa81daa5793f1ab3ff";
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "himansu9861";
+  const APP_ID = "test";
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = "face-detection";
+  // const MODEL_ID = "color-recognition";
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+  return requestOptions;
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+    };
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    // console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
+  onInputChange = (event) => {
+    // console.log(event); only for test
+    // console.log(event.target.value); only for test
+    // console.log({ input: event.target.value });
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    this.setState({ imageUrl: this.state.input });
+    // app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch(
+      "https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs",
+      returnClarifaiRequestOptions(this.state.input)
+    )
+      .then((response) => response.json())
+      .then((response) =>
+        // console.log(response);
+        // console.log(response);
+        // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
   particlesInit = async (engine) => {
     console.log(engine);
     await loadSlim(engine);
@@ -35,24 +134,24 @@ class App extends Component {
             interactivity: {
               events: {
                 onClick: {
-                  enable: true,
+                  enable: false,
                   mode: "push",
                 },
                 onHover: {
-                  enable: true,
+                  enable: false,
                   mode: "repulse",
                 },
                 resize: true,
               },
-              modes: {
-                push: {
-                  quantity: 4,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                },
-              },
+              // modes: {
+              //   push: {
+              //     quantity: 4,
+              //   },
+              //   repulse: {
+              //     distance: 200,
+              //     duration: 0.4,
+              //   },
+              // },
             },
             particles: {
               color: {
@@ -98,8 +197,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
